f8c8e7147b0069c9c445a0bcc6626c48c498a7d6
redashu <ashutoshh@linux.com>
2021-07-05 13:04:36 +0530

    shell first hal

---
+++ hello.sh
@@ -0,0 +1,22 @@
+#!/bin//bash
+
+echo "Hello world";
+sleep 2
+#echo  "Hello WORLD"
+
+#echo  'Hello world'
+sleep 5
+echo  hello world
+
+# to run any command inside shell script 
+
+date
+sleep 2 
+cal 
+sleep 1
+whoami
+sleep 1
+pwd
+
+
+
